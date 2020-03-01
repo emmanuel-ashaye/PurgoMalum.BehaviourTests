395e983b17f5078393f60f8a5c4a2053d247ce75
Emmanuel Ashaye <emmanuelashaye@emmanuels-mbp.home>
2020-03-01 16:51:00 +0000

    reporter

--- CreateReport.sh
+++ CreateReport.sh
@@ -1,2 +1,3 @@
-mono ReportUnit.1.2.1/tools/ReportUnit.exe DockerResults DockerResults/1.2.1 && \
-mono ReportUnit.1.5.0-beta1/tools/ReportUnit.exe DockerResults DockerResults/1.5.0
+#mono ReportUnit.1.2.1/tools/ReportUnit.exe DockerResults DockerResults/1.2.1 && \
+#mono ReportUnit.1.5.0-beta1/tools/ReportUnit.exe DockerResults DockerResults/1.5.0
+mono /extent.0.0.3/tools/extent.exe -d /DockerResults -o /DockerResults/ --merge
